a7b18c1bd806978a72fdf50339a524ef9f0ec1d3
wendychaung <101249461+doraemonSakura@users.noreply.github.com>
2023-12-28 20:59:11 +0800

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -253,7 +253,7 @@ export default function Home({cluster}) {
                         title: 'Upload Image',
                     },
                     {
-                        title: 'Generate Json',
+                        title: 'Generate JSON',
                     },
                     {
                         title: 'Create NFT',
